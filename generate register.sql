9e56c8302b50409d1a4757748e3e83e7b562310a
Jamie Atkinson <jamie@atkinson.io>
2017-10-24 15:31:27 +0100

    added latest version of the charity register, approx 50% of addresses have been matched to a UPRN

--- generate register.sql
+++ generate register.sql
@@ -1,15 +1,22 @@
 SELECT 
-    CONCAT(extract_charity.regno,
-            `extract_charity`.`regno`,
-            '-',
-            extract_charity.subno) AS 'charity',
-    `extract_charity`.`name`,
-    `extract_charity`.`aob` AS 'operating area',
-    `extract_charity`.`postcode` AS 'address',
-    `extract_registration`.`regdate` AS 'start-date',
-    `extract_registration`.`remdate` AS 'end-date'
+    ANY_VALUE(CONCAT(`extract_charity`.`regno`,
+                    '-',
+                    extract_charity.subno)) AS 'charity',
+    ANY_VALUE(`extract_charity`.`name`) AS 'name',
+    ANY_VALUE(`extract_main_charity`.coyno) AS 'company',
+    GROUP_CONCAT(DISTINCT `extract_class`.`class`
+        ORDER BY extract_class.class DESC
+        SEPARATOR ';') AS 'class',
+    ANY_VALUE(`extract_charity`.`postcode`) AS 'address',
+    ANY_VALUE(`extract_registration`.`regdate`) AS 'start-date',
+    ANY_VALUE(`extract_registration`.`remdate`) AS 'end-date'
 FROM
     `charity`.`extract_charity`
-        LEFT OUTER JOIN
+         left OUTER JOIN
     `charity`.`extract_registration` ON `extract_charity`.`regno` = `extract_registration`.`regno`
         AND `extract_charity`.`subno` = `extract_registration`.`subno`
+        LEFT OUTER JOIN
+    `charity`.`extract_class` ON `extract_charity`.`regno` = `extract_class`.`regno`
+        LEFT OUTER JOIN
+    `charity`.`extract_main_charity` ON extract_charity.regno = extract_main_charity.regno
+GROUP BY extract_charity.regno, extract_charity.subno, extract_registration.regdate, extract_registration.remdate
