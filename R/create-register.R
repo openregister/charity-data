d01c112db7ecd894bb9918e952a9ce9a602f0816
Duncan Garmonsway <nacnudus@gmail.com>
2018-03-14 17:29:09 +0000

    Begin checking data quality

--- R/create-register.R
+++ R/create-register.R
@@ -3,7 +3,7 @@
 library(tidyverse)
 library(here)
 
-# Import the data
+# Import the data --------------------------------------------------------------
 
 acct_submit_path <- here("lists", "extract_acct_submit.bcp")
 aoo_ref_path <- here("lists", "extract_aoo_ref.bcp")
@@ -52,3 +52,75 @@ partb <- read_tsv(partb_path, col_names = FALSE, quote = "")
 registration <- read_tsv(registration_path, col_names = registration_colnames, quote = "")
 remove_ref <- read_tsv(remove_ref_path, col_names = remove_ref_colnames, quote = "")
 trustee <- read_tsv(trustee_path, col_names = trustee_colnames, quote = "")
+
+# Boring checks ----------------------------------------------------------------
+
+# Are all charities distinct? Yes, by regno & subno
+nrow(charity) == nrow(distinct(charity, regno))
+nrow(charity) == nrow(distinct(charity, regno, subno))
+
+map(charity, anyNA)
+
+# One charity has no name
+filter(charity, is.na(name))
+
+# All charities have an orgtype: R = registered, RM = Removed
+# The registration table has each period of registration per charity
+
+# All registrations have a regdate
+map(registration, anyNA)
+
+# Registrations are distinct by regno, subno and regdate
+nrow(registration) == nrow(distinct(registration, regno, subno, regdate))
+
+# But registrations are not quite disttinct by remdate -- can there be overlaps?
+nrow(registration) == nrow(distinct(registration, regno, subno, remdate))
+
+# 13 pairs of registrations overlap.  The remcodes agree within pairs.
+registration %>%
+  group_by(regno, subno, remdate) %>%
+  mutate(n = n()) %>%
+  ungroup() %>%
+  filter(n > 1) %>%
+  print(n = Inf)
+
+# We could combine the overlapping periods like this
+registration <-
+  registration %>%
+  group_by(regno, subno, remdate) %>%
+  summarise(regdate = min(regdate),
+            remcode = first(remcode)) %>%
+  ungroup() %>%
+  select(regno, subno, regdate, remdate, remcode)
+
+# Now there are no overlaps
+registration %>%
+  group_by(regno, subno, remdate) %>%
+  mutate(n = n()) %>%
+  ungroup() %>%
+  filter(n > 1) %>%
+  print(n = Inf)
+
+# 3453 remdates don't have remcodes
+filter(registration, !is.na(remdate), is.na(remcode))
+
+# Do registrations accord with orgtype?
+latest_registration <-
+  registration %>%
+  group_by(regno, subno) %>%
+  arrange(desc(regdate)) %>%
+  slice(1) %>%
+  ungroup()
+
+anti_join(charity, latest_registration, by = c("regno", "subno")) # 0 rows, good
+anti_join(latest_registration, charity, by = c("regno", "subno")) # 0 rows, good
+
+# 550 charity orgtypes don't agree with registrations
+latest_registration %>%
+  transmute(regno,
+            subno,
+            orgtype = if_else(is.na(remdate), "R", "RM")) %>%
+  inner_join(select(charity, regno, subno, orgtype),
+             by = c("regno", "subno")) %>%
+  filter(orgtype.x != orgtype.y) %>%
+  count(orgtype.x)
